8c120cd1f1328710a7e98d2818c00abbf075ff6e
mouskou <mr.kourkzi@hotmail.fr>
2024-10-21 11:11:34 +0200

    Add files via upload

---
+++ Algorithme BLACK JACK.py
@@ -0,0 +1,42 @@
+
+#Créer la fonction "black_jack" avec pour argument le budget
+    # player doit miser une certaine somme
+    # Tant que mise > budget
+        # Redemande au player de miser une certaine somme
+
+    # Créer le jeu_carte (dans une liste par compréhension)
+
+    # Créer une lst player et une lst croupier (lst vide)
+
+    # Créer une boucle avec 2 tours
+        # Lancer la fonction "distribuer" ac pour argument "lst_p" et l'enregistrer dans la lst_p
+        # Lancer la fonction "distribuer" ac pour argument "lst_c" et l'enregistrer dans la lst_c
+
+    # Afficher la main du joueur
+    # Afficher la 1er carte du croupier (la 1er valeur de la lst_c)
+
+    ################ EN ATTENTE ################
+    # Demander au player s'il veut tirer une nouvelle carte
+    # Si oui
+        # Lancer la fonction "distribuer" ac pour argument "lst_p" et l'enregistrer dans la lst_p
+        # Lancer la fonction "verif"
+    # Si non
+        # 
+
+#Créer une fonction "verif" ac pour argument lst_p et lst_c et budget
+    # Si somme(lst_p) > 21
+        # Alors on affiche PERDU et on retire la mise au budget
+    # Sinon si somme(lst_c) > 21 OU somme(lst_p) > somme(lst_c) 
+        # Alors on affiche GAGNER et on ajoute la mise au budget
+    # Sinon si somme(lst_c) > somme(lst_p)
+        # Alors on affiche PERDU et on retire la mise au budget
+    ################# EN ATTENTE ################
+
+
+#Créer une fonction "distribuer" avec pour argument une LISTE
+    # Je récupère une carte de manière aléatoire dans le jeu_carte
+    # Je supprime cette carte du jeu_carte
+    # J'ajoute cette carte dans la LISTE
+    # Retourne la LISTE
+
+    
